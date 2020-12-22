8aed9f34edeae7c7f24d367bfe94f5f3a8c0097b
ArtisanGray <1697@holbertonschool.com>
2020-12-22 02:21:21 +0000

    betty'd monty.h

--- monty.h
+++ monty.h
@@ -20,9 +20,9 @@
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /**
@@ -35,32 +35,23 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+/* extern int nodeval; */
 int nodeval;
 
 void push(stack_t **stack, unsigned int line_number);
-
 void pall(stack_t **stack, unsigned int line_number);
-
 void pint(stack_t **stack, unsigned int line_number);
-
 void pop(stack_t **stack, unsigned int line_number);
-
 void swap(stack_t **stack, unsigned int line_number);
-
 void add(stack_t **stack, unsigned int line_number);
-
 void mul(stack_t **stack, unsigned int line_number);
-
 void nop(stack_t **stack, unsigned int line_number);
-
 int parse(char *line, stack_t **stack, unsigned int line_number);
-
-int chkopcode(char* opcode, stack_t **stack, unsigned int line_number);
-
+int chkopcode(char *opcode, stack_t **stack, unsigned int line_number);
 void free_Stack(stack_t **stack);
 
 #endif /* _MONTY_H_ */
